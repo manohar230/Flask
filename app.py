bb455cf5f893034903293d4f6f7091a543c1e64f
Purella Manohar Sri venkat <manoharvenkat14@gmail.com>
2025-06-18 15:22:01 +0530

    adding

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template,request,redirect
+from config import db_config
+app=Flask(__name__)
+
+def get_db_connection():
+    return mysql.connector.connect(**db_config)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/feedback',methods=['GET','POST'])
+def feedback():
+    if request.method =='POST':
+        name=request.form['name']
+        email=request.form['email']
+        comment=request.form['comment']
+
+
+        conn=get_db_connection()
+        cursor=conn.cursor()
+        cursor.execute("INSERT INTO feedback(studnet_name,email,comment) VALUES(%s.%s,%s)" ,(name,email,comment))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect('/')
+    return render_template('feedback.html')
+@app.route('/admin')
+def admin():
+    conn=get_db_connection()
+    cursor=conn.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM feedback ORDER BY SUBMITTED_AT DESC ')
+    feedbacks=cursor.fetchall()
+    conn.close()
+    return render_template('admin.html',feedbacks=feedbacks)
+if __name__=='__maim__':
+    app.run(debug=True)
---
+++ database/feedback.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS feedback_db;
+
+USE feedback_db;
+
+CREATE TABLE IF NOT EXISTS feedback(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    Student_name VARCHAR(100),
+    Emial VARCHAR(100),
+    comments TEXT,
+    submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
